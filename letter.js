6b4f62d16609d6c31c04a3b2737a53fb195cebba
RMENS <icgc@RMs-MacBook-Pro.fios-router.home>
2016-01-21 17:36:20 -0500

    initial commit with letter.js file created containing constructor function code

---
+++ letter.js
@@ -0,0 +1,16 @@
+module.exports = function() {
+
+	function Letter(let) {
+		var charac = let;
+		appear = false;
+
+			letterRender(){
+			if(charac === false) {
+			return("_")
+		} else {
+			return("charac")
+		}
+	}
+
+};
+}
