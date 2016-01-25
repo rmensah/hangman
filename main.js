04cc5e69ea5b34d4967c3039adae076181d1b548
RMENS <icgc@RMs-MacBook-Pro.fios-router.home>
2016-01-25 18:00:14 -0500

    completed writing game in main.js need to further debug code

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ prompt.start();
 var Word = require('./word.js');
 
 var game = {
-	wordBank : 
+	wordBank : {" "}; 
 	guessesRemaining : "10";
 	currentWrd :"null";
 
@@ -15,5 +15,44 @@ var game = {
 
 		var new Word = (wordBank)
 		new Word = currentWrd
+
 	}
+	// called .getLets() method on the currentWrd object (will get back to code here later)
+
+	keepPromptingUser();
+
+	function keepPromptingUser() {
+		var self = this;
+		prompt.get(guessLetter, function(err, result) {
+			console.log("The Letter or space you guessed is" + result.guessLetter);
+			var findHowManyOfUserGuess = currentWrd.checkIfLetterFound(result.guessLetter);
+				if(findHowManyOfUserGuess === 0) {
+					console.log("You gussed wrong~!")
+					guessesRemaining --;
+				}
+				else {
+					console.log("You guessed right!")
+
+					//check if user has won the game
+
+					if (currentWrd.didWeFindTheWord() = true) {
+						console.log("You Won!!!")
+						return 1
+					}
+				}
+
+		})
+	}
+	console.log (Guesses remaining: '+ value of guessesRemaining');
+	console.log( wordRender(this.currentWrd));
+	if(guessesRemaining > 0 && currentWrd.found == false) {
+		keepPromptingUser()
+	} else {
+		if( guessesRemaining === 0) {
+			console.log("Gave over bro")
+		} else {
+			console.log(this.wordRender(currentWrd))
+		}
+	}
+	game.startGame()
 }
